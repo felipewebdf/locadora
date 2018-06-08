70a0c6e9cf890d3114856518dcbcb542620fb4e5
felipewebdf <felipegunsf@gmail.com>
2018-06-08 00:50:17 -0300

    init project

---
+++ app/Http/Controllers/Api/Profile/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\Profile;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    public function test(Request $params)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+        dd($user);
+    }
+}
